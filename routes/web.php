8dee6bfdc9ebd39a6dcb54ac4f9718a6afc25683
Albert Mírek <albert.mirek@seznam.cz>
2020-06-28 20:43:54 +0200

    Added login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::resource('/meals', 'MealsController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/', function (){
+
+    if(Auth::check()){
+
+        return 'the user is logged in';
+    }
+
+//    $username = 'sada';
+//    $password ='dasda';
+
+//    if (Auth::attempt(['username'=>$username, 'password'=>$password])){
+//        return redirect()->intended('/admin');
+//    }
+
 });
+
+
